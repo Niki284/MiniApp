28b22073d2594a9568e3edfd55122ac26475c4c2
NikitaDymovich <nikita.dymovich@student.odisee.be>
2023-04-24 11:37:08 +0200

    blof make

---
+++ mini-app/src/components/Lijst.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Lijst() {
+  return (
+    <div className='mini-content'>
+        <h1>Lijst</h1> 
+        <ul className='mini__list'>
+            <li><a href="https://hi.me">click me</a></li>
+            <li><a href="https://hello.me">hello</a></li>
+            <li><a href="https://bye.me">goodbye</a></li>
+            <li><a href="https://important.me">click me</a></li>
+            <li><a href="https://yow.me">klik mij</a></li>
+        </ul>
+
+        <div className="wrapper">mark</div>
+        <div className="container">tim</div>
+        <div className="section">adriaan</div>
+        <div className="wide">larlo</div>
+        <div className="wrapper">kuki</div>
+
+
+    </div>
+  )
+}
--- mini-app/src/pages/About.jsx
+++ mini-app/src/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Lijst from '../components/Lijst';
+import '../Blog.css'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <h1>About</h1> 
+    <Lijst />
+    </div>
   )
 }
